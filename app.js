2f22cea71af83515a454cd95f16a6ca8c241686e
Tyler Lowe <tlowe3420@gmail.com>
2023-05-04 09:25:22 -0500

    Setting up function to push added employees to array

---
+++ app.js
@@ -0,0 +1,15 @@
+let employee = [
+
+]
+
+function newEmployee(firstName, lastName, ID, JobTitle, annualSalary) {
+    // console.log(employee);
+    employee.push({firstName, lastName, ID, JobTitle, annualSalary} )
+    // console.log(employee);
+}
+
+
+newEmployee('Tyler', 'Lowe', 4521, 'Developer', 120000);
+newEmployee('sarah', 'turd', 3434, 'HR', 50000);
+newEmployee('steve', 'dunker', 2222, 'Waterboy', 200000);
+console.log(employee);
